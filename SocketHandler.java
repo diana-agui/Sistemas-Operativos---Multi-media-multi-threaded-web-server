1c1db4ba9fe90192513c3c6a4ab57b85d833162a
Diana <aztecvoyagediana@gmail.com>
2026-04-21 15:37:59 -0700

    Update SocketHandler.java

--- SocketHandler.java
+++ SocketHandler.java
@@ -1,19 +1,21 @@
 import java.net.*;
 
 public class SocketHandler {
-    
+
     void main() throws Exception {
         ServerSocket server = new ServerSocket(8080);
         ThreadPool pool = new ThreadPool(4, 10);
-        
+
         while (true) {
             Socket connection = server.accept();
-            pool.submit(() -> {
-                try {
-                    connection.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\ndaoms world".getBytes());
-                    connection.close();
-                } catch (Exception e) {
-                    System.err.println("error x_X " );
+            pool.submit(new job() {
+                public void execute() {
+                    try {
+                        connection.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\ndaoms world".getBytes());
+                        connection.close();
+                    } catch (Exception e) {
+                        System.err.println("error x_X ");
+                    }
                 }
             });
         }
